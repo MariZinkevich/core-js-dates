fac7e01b52f84689d7effca16fe518341dafc4b8
Mary Zin <mary22091991mary@gmail.com>
2024-04-05 20:37:50 +0300

    feat: solution isLeapYear

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -260,8 +260,10 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const newDate = date;
+  newDate.setDate(0);
+  return newDate.getDate() === 29;
 }
 
 module.exports = {
